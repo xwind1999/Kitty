c9b93e62ab3dff65c6ce7913b901bb5828f833ed
Your Name <you@example.com>
2020-11-27 17:04:00 +0700

    Add Css

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,25 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 class QuestionController extends AbstractController
 {
     /**
-     * @Route("/")
-     * @return Response
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('What a bewitching controller we have conjured!');
+        return $this->render('question/homepage.html.twig');
     }
 
     /**
      * @Route("/questions/{slug}")
      * @param $slug
-     * @return Response
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function show($slug)
     {
