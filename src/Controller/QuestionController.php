bc80cd221cf3993792b56925cffc7985ee3aba76
Your Name <you@example.com>
2020-12-02 14:13:58 +0700

    Install ORM

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
@@ -13,24 +16,35 @@ class QuestionController extends AbstractController
      */
     public function homepage()
     {
+//        $html = $twigEnvironment->render('question/homepage.html.twig');
+//        return new Response($html);
         return $this->render('question/homepage.html.twig');
     }
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      * @param $slug
+     * @param MarkdownParserInterface $markdownParser
+     * @param CacheInterface $cache
      * @return Response
      */
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
     {
         $answers = [
             'Make sure your cat is sitting purrrfectly still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
+        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
 
+        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser) {
+            return $markdownParser->transformMarkdown($questionText);
+        });
+
+        dump($cache);
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
+            'questionText' => $parsedQuestionText,
             'answers' => $answers,
         ]);
     }
